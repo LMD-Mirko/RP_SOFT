1225211acac50967b65fa36329aa414f87896b63
Sebastian230107 <gabrielsebastiants@gmail.com>
2025-11-05 14:17:28 -0500

    fix: :bug: conflict correction
    I corrected the conflicts when performing the merge

--- src/features/evaluacion-360/index.jsx
+++ src/features/evaluacion-360/index.jsx
@@ -2,10 +2,6 @@ import { Routes, Route } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { EvaluacionesList } from './pages/EvaluacionesList'
 import { Evaluacion360 } from './pages/Evaluacion360'
-<<<<<<< HEAD
-=======
-import { EvaluacionTecnica } from './pages/EvaluacionTecnica'
->>>>>>> main
 
 export function Evaluacion360Index() {
   return (
@@ -13,10 +9,6 @@ export function Evaluacion360Index() {
       <Routes>
         <Route index element={<EvaluacionesList />} />
         <Route path="evaluacion-360" element={<Evaluacion360 />} />
-<<<<<<< HEAD
-=======
-        <Route path="evaluacion-tecnica" element={<EvaluacionTecnica />} /> 
->>>>>>> main
       </Routes>
     </Layout>
   )
